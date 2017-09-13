5192b1d4501b3e61eaca72a9c0c3a10a7da38ac7
msreeramyaa <31915343+msreeramyaa@users.noreply.github.com>
2017-09-13 15:05:49 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+Scanner sn=new Scanner(System.in);
+
+    n=sn.nextInt();
+
+     if(n==1)
+      {
+    
+    setstudent();  
+      }
+
+     if(n==2)
+      {
+       getstudent();
+      }
+     if(n==3)
+      {
+       getbirthDate();
+      } 
+    if(n==4)
+      {
+      getavgMarks();
+       }
+    
+}
+//main function ends
+
+
+}
+
+
+}
